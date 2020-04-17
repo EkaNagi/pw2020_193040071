789c7a28bce685594d650fd04418230d1db811a0
Tresna Eka Widiana <ideal.nagi@gmail.com>
2020-04-17 15:06:23 +0700

    Mengganti database mahasiswa & menambahkan latihan pertemuan 10

---
+++ kuliah/pertemuan10/detail.php
@@ -0,0 +1,32 @@
+<?php
+require 'functions.php';
+
+// Ambil id dari url
+$id = $_GET['id'];
+
+// Query mahasiswa berdasarakan id
+$mhs = query("SELECT * FROM mahasiswa WHERE id = $id");
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Detail Mahasiswa</title>
+</head>
+
+<body>
+  <h3>Detail Mahasiswa</h3>
+  <ul>
+    <li><img src="img/<?= $mhs['gambar']; ?>" alt=""></li>
+    <li>NRP : <?= $mhs['nrp']; ?></li>
+    <li>Nama : <?= $mhs['nama']; ?></li>
+    <li>Email : <?= $mhs['email']; ?></li>
+    <li>Jurusan : <?= $mhs['jurusan']; ?></li>
+    <li><a href="">Ubah</a> | <a href="">Hapus</a></li>
+    <li><a href="">Kembali ke daftar mahasiswa</a></li>
+  </ul>
+</body>
+
+</html>
--- kuliah/pertemuan9/pw_193040071.sql
+++ kuliah/pertemuan9/pw_193040071.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 17, 2020 at 08:31 AM
+-- Generation Time: Apr 17, 2020 at 10:04 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.2.26
 
@@ -30,6 +30,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `mahasiswa` (
   `id` int(11) NOT NULL,
+  `gambar` varchar(30) DEFAULT NULL,
   `nama` varchar(30) NOT NULL,
   `nrp` int(9) NOT NULL,
   `email` varchar(30) NOT NULL,
@@ -40,17 +41,17 @@ CREATE TABLE `mahasiswa` (
 -- Dumping data for table `mahasiswa`
 --
 
-INSERT INTO `mahasiswa` (`id`, `nama`, `nrp`, `email`, `jurusan`) VALUES
-(1, 'Merlin Gunawan', 193040001, 'merlinshinu@unpas.ac.id', 'Teknik Informatika'),
-(2, 'Valkyrie Virra', 193040002, 'valkyriebebeb@unpas.ac.id', 'Teknik Informatika'),
-(3, 'Mang Ozymandias', 193040003, 'dajakudajaku@unpas.ac.id', 'Teknik Informatika'),
-(4, 'Gilgamesh Maiboi', 193040004, 'castercritrateup@unpas.ac.id', 'Teknik Informatika'),
-(5, 'Ishtar Gaguna', 193040005, 'ishtarnub@unpas.ac.id', 'Teknik Informatika'),
-(6, 'Qin Liangyu', 193040006, 'qinmybebeb@unpas.ac.id', 'Teknik Informatika'),
-(7, 'Lanling Wang', 193040007, 'lanlanling@unpas.ac.id', 'Teknik Informatika'),
-(8, 'Richard George', 193040008, 'richardou@unpas.ac.id', 'Teknik Informatika'),
-(9, 'Enkidu Elish', 193040009, 'enkidunothuman@unpas.ac.id', 'Teknik Informatika'),
-(10, 'Hans Andersen', 193040010, 'hansthebook@unpas.ac.id', 'Teknik Informatika');
+INSERT INTO `mahasiswa` (`id`, `gambar`, `nama`, `nrp`, `email`, `jurusan`) VALUES
+(1, 'gambar1.png', 'Merlin Gunawan', 193040001, 'merlinshinu@unpas.ac.id', 'Teknik Informatika'),
+(2, 'gambar2.png', 'Valkyrie Virra', 193040002, 'valkyriebebeb@unpas.ac.id', 'Teknik Informatika'),
+(3, 'gambar3.png', 'Mang Ozymandias', 193040003, 'dajakudajaku@unpas.ac.id', 'Teknik Informatika'),
+(4, 'gambar4.png', 'Gilgamesh Maiboi', 193040004, 'castercritrateup@unpas.ac.id', 'Teknik Informatika'),
+(5, 'gambar5.png', 'Ishtar Gaguna', 193040005, 'ishtarnub@unpas.ac.id', 'Teknik Informatika'),
+(6, 'gambar6.png', 'Qin Liangyu', 193040006, 'qinmybebeb@unpas.ac.id', 'Teknik Informatika'),
+(7, 'gambar7.png', 'Lanling Wang', 193040007, 'lanlanling@unpas.ac.id', 'Teknik Informatika'),
+(8, 'gambar8.png', 'Richard George', 193040008, 'richardou@unpas.ac.id', 'Teknik Informatika'),
+(9, 'gambar9.png', 'Enkidu Elish', 193040009, 'enkidunothuman@unpas.ac.id', 'Teknik Informatika'),
+(10, 'gambar10.png', 'Hans Andersen', 193040010, 'hansthebook@unpas.ac.id', 'Teknik Informatika');
 
 --
 -- Indexes for dumped tables
@@ -70,7 +71,7 @@ ALTER TABLE `mahasiswa`
 -- AUTO_INCREMENT for table `mahasiswa`
 --
 ALTER TABLE `mahasiswa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
